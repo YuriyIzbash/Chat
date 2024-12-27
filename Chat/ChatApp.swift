1d15f4c9b4147515372846bed00de91fe82e64d0
YuriyIzbash <izbash91@gmail.com>
2024-12-27 21:39:09 +0100

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by YURIY IZBASH on 27. 12. 24.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chat
+//
+//  Created by YURIY IZBASH on 27. 12. 24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        HStack {
+            Text("Hello, World!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
